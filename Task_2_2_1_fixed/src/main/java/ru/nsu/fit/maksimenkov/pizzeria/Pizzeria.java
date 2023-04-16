9345e3364b51455d17dd6268cecebb146035d55f
grilld9 <m.maksimenkov@g.nsu.ru>
2023-04-16 13:24:48 +0700

    init commit.

---
+++ Task_2_2_1_fixed/src/main/java/ru/nsu/fit/maksimenkov/pizzeria/Pizzeria.java
@@ -0,0 +1,66 @@
+package ru.nsu.fit.maksimenkov.pizzeria;
+
+import ru.nsu.fit.maksimenkov.pizzeria.bakers.BakerWorker;
+import ru.nsu.fit.maksimenkov.pizzeria.couriers.CourierWorker;
+
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class Pizzeria {
+
+    private final Long nBaker;
+    private final List<Integer> trunkCaps;
+    private final BlockingQueue<Order> mainQueue = new LinkedBlockingQueue<>(100);
+    private final BlockingQueue<String> loggerQueue = new LinkedBlockingQueue<>();
+
+    private final BlockingQueue<Order> warehouse;
+    private Boolean isInitDone = false;
+    private OrderGenerator orderGenerator = new OrderGenerator(mainQueue);
+
+    public Pizzeria(Long nBaker, Long warehouseSize, List<Integer> trunkCaps) {
+        this.nBaker = nBaker;
+        this.trunkCaps = trunkCaps;
+        warehouse = new LinkedBlockingQueue<>(Math.toIntExact(warehouseSize));
+    }
+
+    private void init() {
+        initLogger();
+        initBakers();
+        initCouriers();
+        isInitDone = true;
+    }
+
+    private void initBakers() {
+        for (int i = 0; i < nBaker; i++) {
+            BakerWorker baker = new BakerWorker(mainQueue, warehouse, loggerQueue);
+            baker.start();
+        }
+    }
+
+    private void initCouriers() {
+        for (Integer cap : trunkCaps) {
+            CourierWorker courier = new CourierWorker(warehouse, cap, loggerQueue);
+            courier.start();
+        }
+    }
+
+    private void initLogger() {
+        Logger logger = new Logger(loggerQueue);
+        logger.start();
+    }
+
+    public void addNewOrder(Order order) throws InterruptedException {
+        if (!isInitDone) {
+            init();
+        }
+        mainQueue.put(order);
+    }
+
+    public void start() throws InterruptedException {
+        if (!isInitDone) {
+            init();
+        }
+        orderGenerator.start();
+    }
+}
---
+++ Task_2_2_1_fixed/src/main/java/ru/nsu/fit/maksimenkov/pizzeria/bakers/BakerWorker.java
@@ -0,0 +1,48 @@
+package ru.nsu.fit.maksimenkov.pizzeria.bakers;
+
+import ru.nsu.fit.maksimenkov.pizzeria.Order;
+import ru.nsu.fit.maksimenkov.pizzeria.Worker;
+
+import java.util.concurrent.BlockingQueue;
+
+
+public class BakerWorker extends Thread implements Worker {
+
+    private final BlockingQueue<Order> mainQueue;
+    private final BlockingQueue<String> loggerQueue;
+    private final BlockingQueue<Order> warehouse;
+
+    public BakerWorker(BlockingQueue<Order> mainQueue, BlockingQueue<Order> warehouse,
+        BlockingQueue<String> loggerQueue) {
+        this.mainQueue = mainQueue;
+        this.warehouse = warehouse;
+        this.loggerQueue = loggerQueue;
+    }
+
+    @Override
+    public void run() {
+        while (!this.isInterrupted()) {
+            try {
+                Order order = takeOrder();
+                putOrder(order);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public Order takeOrder() throws InterruptedException {
+        Order nextOrder = mainQueue.take();
+        loggerQueue.put(nextOrder.getOrderNumber() + ", cooking started");
+        sleep(1000 * 5);
+        loggerQueue.put(nextOrder.getOrderNumber() + ", cooked");
+        return nextOrder;
+    }
+
+    @Override
+    public void putOrder(Order order) throws InterruptedException {
+        warehouse.put(order);
+        loggerQueue.put(order.getOrderNumber() + ", stored into warehouse");
+    }
+}
