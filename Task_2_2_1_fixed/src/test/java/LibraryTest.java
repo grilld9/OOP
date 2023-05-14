4d47bb79c4dbdaab60cdfdbebb3c2e19dc66d95e
grilld9 <m.maksimenkov@g.nsu.ru>
2023-05-14 11:18:00 +0700

    add tests

---
+++ Task_2_2_1_fixed/src/test/java/LibraryTest.java
@@ -0,0 +1,41 @@
+package ru.nsu.fit.maksimenkov.pizzeria;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
+import static java.lang.Thread.sleep;
+
+public class LibraryTest {
+
+    @Test
+    public void test() throws InterruptedException {
+        Long nBaker = 3L;
+        Long warehouseSize = 2L;
+        List<Integer> trunkCaps = Arrays.asList(1, 2);
+        Pizzeria pizzeria = new Pizzeria(nBaker, warehouseSize, trunkCaps);
+        pizzeria.start();
+        sleep(50000);
+    }
+
+    @Test
+    public void testWithJson() throws IOException, ParseException, InterruptedException {
+        Object o = new JSONParser().parse(new FileReader(
+            "src/main/resources/info.json"));
+        JSONObject j = (JSONObject) o;
+        Long nBaker = (Long) j.get("nBaker");
+        Long nCourier = (Long) j.get("nCourier");
+        Long warehouse = (Long) j.get("warehouse");
+        List<Integer> trunkCaps = Arrays.asList(1, 2);
+        Pizzeria pizzeria = new Pizzeria(nBaker, warehouse, trunkCaps);
+        pizzeria.start();
+        sleep(50000);
+    }
+}
